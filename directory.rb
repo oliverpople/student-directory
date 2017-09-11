7ab90c81bad05ec4bdfa8ec619025008aa5ab003
oliverpople <oliverpople@gmail.com>
2017-09-11 15:25:37 +0100

    Adding additional commnents to list of students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# First we print a list of students
 puts "The students of Villians Academy"
 puts "______________"
 puts "Dr. Hannibal Lectur"
@@ -11,6 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# Finally we print an the total number of students
 print "Overall, we have "
+# It's important that print() doesn't add a new line characters
 print 11
 puts " great students"
