8f6bb895dcc21e32a8be3552c10816d61473d808
oliverpople <oliverpople@gmail.com>
2017-09-12 09:10:40 +0100

    Adding interactive user input to student list.

--- directory.rb
+++ directory.rb
@@ -1,21 +1,26 @@
-# let's put all students into an array
-students = [
-   {name: "Dr. Hannibal Lectur", cohort: :november},
-   {name: "Darth Vader", cohort: :november},
-   {name: "Nurse Ratched", cohort: :november},
-   {name: "Michael Corleone", cohort: :november},
-   {name: "Alex Delarge", cohort: :november},
-   {name: "The Wicked Witch of the West", cohort: :november},
-   {name: "Terminator", cohort: :november},
-   {name: "Freddy Krueger", cohort: :november},
-   {name: "The Joker", cohort: :november},
-   {name: "Joffrey Baratheon", cohort: :november},
-   {name: "Norman Bates", cohort: :november}
- ]
-# and then print them
+# let's put all students into an interactive array
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just the hit return twice"
+  #create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
+# and then print them
 def print_header
-  puts "the students of Villians Academy"
+  puts "The students of Villians Academy"
   puts "______________"
 end
 
@@ -29,6 +34,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 # nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
