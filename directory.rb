5e09fd48799282f2902866f7c19ca899412463f1
oliverpople <oliverpople@gmail.com>
2017-09-12 18:15:26 +0100

    Adding answer to excercise 8.5

--- directory.rb
+++ directory.rb
@@ -1,20 +1,22 @@
 # let's put all students into an interactive array
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names of the students" +
+  " followed by their favourite hobby, country of birth and height(cm)."
+  + " Please seperate each by a comma"
   puts "To finish, just the hit return twice"
   #create an empty array
   students = []
-  # get the first name
-  name = gets.chomp
+  # get the first name + personal details from user
+  name, hobby, country, height = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, hobby: hobby, country: country, height: height, cohort: :november}
     puts "Now we have #{students.count} students"
-    # get another name from the user
-    name = gets.chomp
+    # get another name + personal from the user
+    name, hobby, country, height = gets.chomp
   end
-  # return the array of students
+  # return the array of students + personal details
   students
 end
 
